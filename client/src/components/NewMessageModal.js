c61336e73cdced7a5c3c2f486b1a411fae09ace3
corina <pixmin@outlook.com>
2022-10-19 13:05:50 -0700

    Added handle form submit functions

--- client/src/components/NewMessageModal.js
+++ client/src/components/NewMessageModal.js
@@ -3,6 +3,13 @@ import { Dialog } from '@headlessui/react'
 import { Form } from 'react-router-dom';
 
 function NewMessageModal({ isOpen, setIsOpen }) {
+  const [message, setMessage] = useState()
+  const [name, setName] = useState()
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(message)
+  }
 
   return (
     <>
@@ -11,14 +18,14 @@ function NewMessageModal({ isOpen, setIsOpen }) {
         <Dialog.Panel>
           <Dialog.Title>Add a new message</Dialog.Title>
           <Dialog.Description>
-            <form>
+            <form onSubmit={handleSubmit}>
               <label>
                 Message:
-                <textarea />
+                <textarea value={message} onChange={e => setMessage(e.target.value)}/>
               </label>
               <label>
                 Name:
-                <input type="text"/>
+                <input type="text" value={name} onChange={e => setName(e.target.value)}/>
               </label>
               
               <input type="submit" value="Submit" />
@@ -33,19 +40,5 @@ function NewMessageModal({ isOpen, setIsOpen }) {
 
   )
 }
-
-
-// function NewMessageModal(){
-//     return (
-//         <div id="myModal" class="modal">
-
-//             <div class="modal-content">
-//                 <span class="close">&times;</span>
-//                 <p>Some text in the Modal..</p>
-//             </div>
-
-//         </div>
-//     );
-// }
 
 export default NewMessageModal;
