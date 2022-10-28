78c83bb62f2022e3098437fa82cfb279ccbc5107
corina <pixmin@outlook.com>
2022-10-27 18:09:40 -0700

    Connect register form to firebase

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,6 +2,10 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
+import {
+    getAuth,
+    createUserWithEmailAndPassword,
+  } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -14,5 +18,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig); 
-// export default app;
+const app = initializeApp(firebaseConfig);
+// const auth = getAuth(app);
+// const db = getFirestore(app);
+export default app;
+
