63a5a3bca8e6ef360bbd71c9b2022ae4504a58eb
corina <pixmin@outlook.com>
2022-10-28 23:50:50 -0700

    Login form and connect to firebase auth

---
+++ client/src/components/LogIn.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react'
+import { Dialog } from '@headlessui/react'
+import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
+import app from "../firebase";
+
+function Login({ isOpen, setIsOpen, user }) {
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  })
+
+  const auth = getAuth(app);
+
+  function updateForm(value) {
+    return setForm((prev) => {
+      return { ...prev, ...value };
+    });
+  }
+
+  async function handleSubmit(e){
+    e.preventDefault()
+    signInWithEmailAndPassword(auth, form.email, form.password)
+      .then((userCredential) => {
+        // Signed in 
+        // const user = userCredential.user;
+        alert("signed in!")
+      })
+      .catch((error) => {
+        // const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage)
+      });
+    
+  }
+
+
+  return (
+    <>
+      <button onClick={buttonClick}>Login</button>
+      <Dialog open={isOpen} onClose={() => setIsOpen(false)}>
+        <Dialog.Panel>
+          <Dialog.Title>Login</Dialog.Title>
+          <Dialog.Description>
+            <form onSubmit={handleSubmit}>
+              <label>
+                Email:
+                <input value={form.email} onChange={e => updateForm({email: e.target.value})}/>
+              </label>
+              <label>
+                Password:
+                <input type="password" value={form.password} onChange={e => updateForm({password: e.target.value})}/>
+              </label>
+              
+              <input type="submit" value="Submit" />
+            </form>
+          </Dialog.Description>
+          
+          <button onClick={() => setIsOpen(false)}>Cancel</button>
+        </Dialog.Panel>
+      </Dialog>
+    </>
+
+  )
+}
+
+export default Login;
--- client/src/components/Message.js
+++ client/src/components/Message.js
@@ -19,7 +19,6 @@ export default function Message(){
         }
     
         getRecords();
-        console.log(records)
     
         return;
     }, [records.length]);
